d7dd37b208f280ae6937e65c3302b1f82114028c
Richard Holzeis <richard.holzeis@at.ibm.com>
2019-06-22 21:46:44 +0200

    init commit

---
+++ go.mod
@@ -0,0 +1,16 @@
+module test
+
+go 1.12
+
+require (
+	github.com/fsouza/go-dockerclient v1.4.1 // indirect
+	github.com/hyperledger/fabric v1.4.1
+	github.com/hyperledger/fabric-amcl v0.0.0-20181230093703-5ccba6eab8d6 // indirect
+	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7 // indirect
+	github.com/spf13/viper v1.4.0 // indirect
+	github.com/sykesm/zap-logfmt v0.0.2 // indirect
+	golang.org/x/crypto v0.0.0-20190621222207-cc06ce4a13d4 // indirect
+	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
+	google.golang.org/genproto v0.0.0-20180831171423-11092d34479b // indirect
+	google.golang.org/grpc v1.21.1 // indirect
+)
---
+++ test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
+)
+
+var logger = shim.NewLogger("test-main")
+
+// Test struct
+type Test struct{}
+
+// Invoke - Called on chaincode invoke. Takes a function name passed and calls that function. Passes the initial arguments passed are passed on to the called function.
+func (test *Test) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	transactionName, _ := stub.GetFunctionAndParameters()
+
+	logger.Infof("Executing transaction '%s'", transactionName)
+
+	return shim.Success(nil)
+}
+
+// Main - main - starts up the chaincode
+func main() {
+	// LogDebug, LogInfo, LogNotice, LogWarning, LogError, LogCritical (Default: LogDebug)
+	logger.SetLevel(shim.LogDebug)
+
+	err := shim.Start(new(Test))
+	if err != nil {
+		logger.Errorf("Error starting Test: %s", err)
+	}
+}
+
+// Init Function - Called when the user deploys the chaincode
+func (test *Test) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	logger.Infof("Chaincode initialized!!")
+
+	return shim.Success(nil)
+}
